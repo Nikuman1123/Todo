9f8b3dd71d8bcd399e4db34db4dae8ccad066c98
Nikuman1123 <shoichi1123@icloud.com>
2025-05-02 22:18:20 +0900

    自動でプルとかはやめるので、関連箇所は全て削除

--- git-pull-safe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-if git diff-index --quiet HEAD --; then
-  echo "作業ツリーはクリーンです。git pull を実行します。"
-  git pull
-else
-  echo "ローカルに未コミットの変更があります。git pull はスキップしました。"
-fi
